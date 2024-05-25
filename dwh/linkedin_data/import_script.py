6d7454e4f5c6156b975af3072a460d933a50d1c1
Ghost <104355357+Knaeckebrothero@users.noreply.github.com>
2024-05-25 19:40:23 +0200

    Added functions to import related profiles

--- dwh/linkedin_data/import_script.py
+++ dwh/linkedin_data/import_script.py
@@ -52,3 +52,39 @@ for doc in documents:
 
     # Insert person data
     person_df.to_sql('FACT_PRF_Person', dwh, if_exists='append', index=False)
+    person_id = pd.read_sql_query("SELECT LAST_INSERT_ID()", dwh).iloc[0, 0]
+
+    # Insert recommendation data
+    if doc.get('recommendations') & len(doc.get('recommendations')) > 0:
+        for rec in doc.get('recommendations'):
+            # Create and insert recommendation
+            pd.DataFrame({
+                'idPerson': person_id,
+                'recommendationText': rec
+            }).to_sql('FACT_PRF_Recommendation', dwh, if_exists='append', index=False)
+    
+    # Insert related profiles
+    if doc.get('people_also_viewed') & len(doc.get('people_also_viewed')) > 0:
+        for related in doc.get('people_also_viewed'):
+            # Create and insert profile
+            pd.DataFrame({
+                'idPerson': person_id,
+                'type': 'viewed',
+                'name': related.get('name'),
+                'location': related.get('location'),
+                'summary': related.get('summary')
+            }).to_sql('FACT_PRF_Recommendation', dwh, if_exists='append', index=False)
+    
+    # Insert related profiles
+    if doc.get('similarly_named_profiles') & len(doc.get('similarly_named_profiles')) > 0:
+        for related in doc.get('similarly_named_profiles'):
+            # Create and insert profile
+            pd.DataFrame({
+                'idPerson': person_id,
+                'type': 'similar',
+                'name': related.get('name'),
+                'location': related.get('location'),
+                'summary': related.get('summary')
+            }).to_sql('FACT_PRF_Recommendation', dwh, if_exists='append', index=False)
+
+    # 
