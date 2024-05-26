2ab994c575babf114faf234737b523215d704862
Ghost <OverlyGenericAddress@pm.me>
2024-05-26 12:16:01 +0200

    Implemented all profile tables except qualification, made change to schema

--- dwh/linkedin_data/convert/profile.py
+++ dwh/linkedin_data/convert/profile.py
@@ -3,6 +3,21 @@ This module contains functions to convert document data to DWH tables.
 
 """
 import pandas as pd
+import datetime
+
+
+def _convert_date(date) -> datetime.date | None:
+    """
+        This function converts a date dictionary from MongoDB to a Python datetime.date object.
+        This date can be inserted into a MySQL DATE column as it inherently matches the required format.
+    
+        :param date: The date dictionary to convert.
+        :return: A date object or None if the date is not complete.
+    """
+    if date and all(k in date for k in ['year', 'month', 'day']):
+        return datetime.date(year=date['year'], month=date['month'], day=date['day'])
+    else:
+        return None
 
 
 def person(document: dict, dimension_key_location: int, dimension_key_origin: int) -> pd.DataFrame:
@@ -36,7 +51,7 @@ def person(document: dict, dimension_key_location: int, dimension_key_origin: in
     return pd.DataFrame([person_dict])
 
 
-def location(document) -> pd.DataFrame:
+def location(document: dict) -> pd.DataFrame:
     """
     This function converts a location to the FACT_LIN_Location table.
 
@@ -54,28 +69,38 @@ def location(document) -> pd.DataFrame:
     # Create and return DataFrame
     return pd.DataFrame([location_dict])
 
-def recommendation(document,) -> pd.DataFrame:
+
+def experience(experience_object: dict, key_person: int, key_duration: int = None, key_institution: int = None):
     """
-    This function converts recommendations to the FACT_PRF_Recommendation table.
+    This function converts qualifications to the FACT_PRF_Qualification table.
 
-    :param document: The document to convert.
-    """
-
-
-
-def related(document) -> pd.DataFrame:
-    """
-    This function converts related profile to the FACT_LIN_Related table.
-
-    :param document: The document to convert.
+    :param key_person: The ID of the person entry in the DWH.
+    :param experience_object: The experience object to convert.
+    :param key_duration: The ID of the duration entry in the DWH.
+    :param key_institution: The ID of the institution entry in the DWH.
     """
 
     # Create and fill the table
-    related_dict = {
-        'idPerson': document.get('idPerson'),
-        'idRelatedPerson': document.get('idRelatedPerson'),
-        'connectionDegree': document.get('connectionDegree')
+    experience_dict = {
+        'idPerson': key_person,
+        'idDuration': key_duration,
+        'idInstitution': key_institution,
+        'type': 'experience',
+        'name': experience_object.get('title'),
+        'description': experience_object.get('description')
     }
 
-    # Create and return DataFrame
-    return pd.DataFrame([related_dict])
+    # Create and fill the duration dimension table
+    duration_dict = {
+        'startDate': _convert_date(experience_object.get('starts_at')),
+        'endDate': _convert_date(experience_object.get('ends_at'))
+    }
+
+    # Create and fill the institution dimension table
+    institution_dict = {
+        'name': experience_object.get('company'),
+        'location': experience_object.get('location')
+    }
+
+    # Create and return DataFrames
+    return pd.DataFrame([experience_dict]), pd.DataFrame([duration_dict]), pd.DataFrame([institution_dict])
