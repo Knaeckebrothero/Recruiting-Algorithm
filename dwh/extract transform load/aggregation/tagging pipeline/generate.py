739c1a0da821c006d5ad7c2dd9d1dbea6ca26149
Ghost <OverlyGenericAddress@pm.me>
2024-08-29 15:21:43 +0200

    Refactored the tagging agent
    - Cleaned imports and init
    - Added and tested websearch
    - Added and tested vector db

--- dwh/extract transform load/aggregation/tagging pipeline/generate.py
+++ dwh/extract transform load/aggregation/tagging pipeline/generate.py
@@ -1,42 +1,43 @@
 """
 This module contains functions to generate attributes for the tagging pipeline.
 """
+import os
+from dotenv import load_dotenv, find_dotenv
+from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain.tools import StructuredTool
+from langchain_chroma import Chroma
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain.agents import create_tool_calling_agent
-from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder, PromptTemplate
-from langchain.agents import AgentExecutor
 
 
-def _format_prompt(system_prompt: str) -> list:
+def _format_prompt(system_prompt: str) -> ChatPromptTemplate:
     """
-    Formats the system prompt as a list of messages to be used in a LangChain agent.
+    Formats the system prompt using langchains template.
 
     :param system_prompt: The system prompt to format.
     :return: The prompt formatted as a list of messages.
     """
-    formatted_messages = [
-        SystemMessagePromptTemplate(
-            prompt=PromptTemplate(input_variables=[], template=system_prompt)
-        ),
-        MessagesPlaceholder(variable_name='chat_history', optional=True),
-        HumanMessagePromptTemplate(
-            prompt=PromptTemplate(input_variables=['input'], template='{input}')
-        ),
-        MessagesPlaceholder(variable_name='agent_scratchpad')
-    ]
-    return formatted_messages
+    return ChatPromptTemplate.from_messages([
+        ("system", system_prompt),
+        ("placeholder", "{chat_history}"),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ])
 
 
 class OpenAiAgent:
     """
     This class defines an OpenAI agent for generating attributes based on the given qualifications.
 
-    :_search_engine: The search engine to use for generating attributes.
-    :_agent: The agent to use for generating attributes
+    The class requires the following environment variables to be set:
+    - TAVILY_API_KEY: The API key for the Tavily AI search engine.
+    - OPENAI_API_KEY: The API key for the OpenAI API.
+    - CHROMA_DB_PATH: The path where the persistent Chroma database should be stored.
     """
     _search_engine: TavilySearchResults
-    _agent: create_tool_calling_agent
+    _vector_database: Chroma
 
     def __init__(self, prompt, model: str = "gpt-4o-mini", temperature: float = 0):
         """
@@ -47,15 +48,89 @@ class OpenAiAgent:
         :param model: The OpenAI model to use for generating attributes.
         :param temperature: The temperature to use for generating attributes.
         """
+        if not (
+                os.getenv("TAVILY_API_KEY") or
+                os.getenv("OPENAI_API_KEY") or
+                os.getenv("CHROMA_DB_PATH")):
+            load_dotenv(find_dotenv())
+            if not (
+                    os.getenv("TAVILY_API_KEY") or
+                    os.getenv("OPENAI_API_KEY") or
+                    os.getenv("CHROMA_DB_PATH")):
+                raise ValueError("Please provide the respective environment variables!")
+
+        # Initialize the search engine
+        self._search_engine = TavilySearchResults()
+
+        # Initialize the vector database
+        self._vector_database = Chroma(
+            persist_directory="./chroma",
+            embedding_function=OpenAIEmbeddings()
+        )
+
+        # Create tools for storing and retrieving data
+        store_key_value = StructuredTool.from_function(
+            func=self._store_data,
+            name="store_data",
+            description="Store a key-value pair in the database."
+        )
+        retrieve_vale_by_key = StructuredTool.from_function(
+            func=self._retrieve_data_search_by_key,
+            name="retrieve_data",
+            description="Retrieve a list of values from the database based on the given key."
+        )
+
+        # Initialize the components
+        tools = [self._search_engine, store_key_value, retrieve_vale_by_key]
         llm = ChatOpenAI(model=model, temperature=temperature)
+        agent = create_tool_calling_agent(llm, tools=tools, prompt=_format_prompt(prompt))
 
-        self.set_search_engine(TavilySearchResults())
-        self.set_agent(llm, _format_prompt(prompt), [])
-        self._agent_executor = AgentExecutor(self._agent, tools=[self._search_engine], verbose=True)
+        # Initialize the agent executor
+        self._agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-    def set_search_engine(self, search_engine: TavilySearchResults):
-        self._search_engine = search_engine
+    def __str__(self):
+        # TODO: Implement a better string representation.
+        return f"OpenAI Agent: {self._agent_executor.agent}"
 
-    def set_agent(self, llm, prompt_messages, tools):
-        self._agent = create_tool_calling_agent(llm, tools, prompt_messages)
+    def _generate(self, text: str) -> str:
+        """
+        Generates a response to the given text.
 
+        :param text:
+        :return: The generated attributes.
+        """
+        return self._agent_executor.invoke({'input': text})['output']
+
+    def _store_data(self, key: str, value: str) -> str:
+        """
+        Stores the given key-value pair in the Chroma database.
+
+        :param key: The key to store the value under.
+        :param value: The value to store.
+        :return: A message indicating the success or failure of the operation.
+        """
+        try:
+            self._vector_database.add_texts([value], metadatas=[{"key": key}])
+            return f"Stored '{key}': '{value}' in the database."
+        except Exception as e:
+            return f"Error storing data: {e}"
+
+    def _retrieve_data_search_by_key(self, key: str) -> str:
+        """
+        Retrieves data from the Chroma database based on the given key.
+
+        :param key: The key to search for.
+        :return: A message indicating the success or failure of the operation.
+        """
+        result = "Retrieved the following data:\n"
+        try:
+            search_results = self._vector_database.similarity_search(key, k=1)
+            if search_results:
+                for r in search_results:
+                    result += f"{r.metadata['key']}: {r.text}\n"
+            else:
+                result = f"No data found for key '{key}'."
+        except Exception as e:
+            result = f"Error retrieving data: {e}"
+        finally:
+            return result
